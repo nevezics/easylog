18d0cf647b92f09b7502057055ca73217ea40f0f
Péter Kalicz <kaliczp@gmail.com>
2021-10-07 17:44:19 +0200

    Simplify script
    Some original importing lines removed

--- Readme.R
+++ Readme.R
@@ -1,16 +1,12 @@
 placename  <- "EGER1"
 placename  <- "EGER2"
+placename  <- "HAZ1"
 
 filename <- dir("../hom_para_NCS", placename)
 assign(placename, read.csv(paste0("../hom_para_NCS/", filename[1]), skip = 1, head = FALSE))
 for(aktfile in filename[-1]) {
     assign(placename, rbind(get(placename), read.csv(paste0("../hom_para_NCS/", aktfile), skip = 1, head = FALSE)))
 }
-
-EGER2 <- read.csv("../hom_para_NCS/EGER2302.txt", skip = 1, head = FALSE)
-
-HAZ1 <- read.csv("../hom_para_NCS/HAZ1302.txt", skip = 1, head = FALSE)
-HAZ2 <- read.csv("../hom_para_NCS/HAZ2302.txt", skip = 1, head = FALSE)
 
 library(xts)
 ttime <- as.POSIXct(EGER1[,2])
