6bd330777a7b7487f28ee50b3a0615ca96dc9086
Nevezi Csenge <nevezi.csenge.12@gmail.com>
2021-10-12 14:56:25 +0200

    szegély összehasonlítás

--- Readme.R
+++ Readme.R
@@ -2,6 +2,7 @@ placename  <- "EGER1"
 placename  <- "EGER2"
 placename  <- "HAZ1"
 placename  <- "HAZ2"
+placename <- "ESZEGELY2"
 
 filename <- dir("../hom_para_NCS", placename)
 assign(placename, read.csv(paste0("../hom_para_NCS/", filename[1]), skip = 1, head = FALSE))
@@ -24,6 +25,10 @@ HAZ1.xts <- xts(HAZ1[,3:5], ttime)
 ttime <- as.POSIXct(HAZ2[,2])
 HAZ2.xts <- xts(HAZ2[,3:5], ttime)
 
+ttime <- as.POSIXct(ESZEGELY2[,2])
+ESZEGELY2.xts <- xts(ESZEGELY2[,3:5], ttime)
+
+
 lines(as.zoo(HAZ1.xts[,1]), col = 3)
 lines(as.zoo(HAZ2.xts[,1]), col = 4)
 
@@ -45,3 +50,8 @@ EGER1napi.xts <- apply.daily(EGER1.xts, mean)
 EGER2napi.xts <- apply.daily(EGER2.xts, mean)
 HAZ2napi.xts <- apply.daily(HAZ2.xts, mean)
 plot(HAZ2napi.xts[,1]-EGER2napi.xts[,1])
+
+summary(ESZEGELY2.xts)
+ESZEGELY2napi.xts <- apply.daily(ESZEGELY2.xts, mean)
+plot(ESZEGELY2napi.xts[,1]-EGER2napi.xts["2021-06-25/",1])
+
