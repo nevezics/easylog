02309b313c13ed7257b2fef3aa5a34d31bc6f00f
Péter Kalicz <kalicz.peter@uni-sopron.hu>
2022-01-05 18:08:19 +0100

    Date as.Date

---
+++ NapiManip.R
@@ -0,0 +1,6 @@
+## Date időre alakítás
+ttESZ2n.idx <- as.Date(index(ESZEGELY2napi.xts))
+ESZ2.D <- xts(coredata(ESZEGELY2napi.xts), order.by = ttESZ2n.idx)
+
+ttE2n.idx <- as.Date(index(EGER2napi.xts))
+E2.D <- xts(coredata(EGER2napi.xts), order.by = ttE2n.idx)
