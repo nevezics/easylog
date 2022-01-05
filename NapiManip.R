43d1296d7149b65282a40cf0cdec9faf107fec49
Péter Kalicz <kalicz.peter@uni-sopron.hu>
2022-01-05 18:14:36 +0100

    Daily differences in the overlapping part

--- NapiManip.R
+++ NapiManip.R
@@ -4,3 +4,7 @@ ESZ2.D <- xts(coredata(ESZEGELY2napi.xts), order.by = ttESZ2n.idx)
 
 ttE2n.idx <- as.Date(index(EGER2napi.xts))
 E2.D <- xts(coredata(EGER2napi.xts), order.by = ttE2n.idx)
+
+
+## Nekem simán képezi a különbséget
+ESZ2.D-E2.D
